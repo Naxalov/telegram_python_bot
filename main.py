baf34e6d965bcca0d7043f09b95f71a6d026df1c
Zarif MAC <uzbzarif@gmail.com>
2020-10-06 20:53:07 +0900

    Final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telegram
 from pprint import pprint
 bot = telegram.Bot(token='1324065101:AAG9GNi2kO942i9tLhy7vmDjAFAeSK4hCbE')
-update = bot.getUpdates()
+update = bot.getUpdates()[-1]
+print(update)
