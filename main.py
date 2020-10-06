9c1927f6ba78ee896c7d4d5ef9d6691695c78c6b
Zarif MAC <uzbzarif@gmail.com>
2020-10-06 20:14:07 +0900

    Loop Message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telegram
 
-bot = telegram.Bot(token='187988593:AAHxj-q6lHxa8cHS4Y8tPTwCUBNML_jkw0U')
-bot.sendMessage(chat_id='86775091', 'Hi!')
+bot = telegram.Bot(token='1324065101:AAG9GNi2kO942i9tLhy7vmDjAFAeSK4hCbE')
+while True:
+    bot.sendMessage(chat_id='86775091', text='Hi!')
