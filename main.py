2b4047769e135e1240c4d093acb3e180a5ca7a6b
Zarif MAC <uzbzarif@gmail.com>
2020-10-06 21:00:39 +0900

    SendPhoto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telegram
 from pprint import pprint
 bot = telegram.Bot(token='1324065101:AAG9GNi2kO942i9tLhy7vmDjAFAeSK4hCbE')
-update = bot.getUpdates()[-1]
-print(update)
+bot.sendPhoto(chat_id='86775091',
+              photo='https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg')
