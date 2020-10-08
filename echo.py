4fb606e0770e9f013c326b7246d745a05ac591ce
Zarif MAC <uzbzarif@gmail.com>
2020-10-08 20:35:03 +0900

    Fultesr

--- echo.py
+++ echo.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 def hello(update, context):
@@ -8,7 +8,7 @@ def hello(update, context):
 updater = Updater(
     '1324065101:AAFC8E5163G83YMG5MJ_X9ay1tvnmZ_6Mds', use_context=True)
 
-updater.dispatcher.add_handler(MessageHandler(hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, hello))
 
 updater.start_polling()
 updater.idle()
