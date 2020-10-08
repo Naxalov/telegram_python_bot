c165f64e6cd227921de855158a199418b165dd0a
Zarif MAC <uzbzarif@gmail.com>
2020-10-08 20:44:49 +0900

    Print(1)

--- echo.py
+++ echo.py
@@ -1,14 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def hello(update, context):
-    update.message.reply_text('Hello World')
+def hi(update, context):
+    print(1)
 
 
-updater = Updater(
-    '1324065101:AAFC8E5163G83YMG5MJ_X9ay1tvnmZ_6Mds', use_context=True)
+updater = Updater('1324065101:AAFC8E5163G83YMG5MJ_X9ay1tvnmZ_6Mds')
 
-updater.dispatcher.add_handler(MessageHandler(Filters.text, hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, hi))
 
 updater.start_polling()
 updater.idle()
