7ca51f51d0679e1886a0db0557638c6e266c2556
Zarif MAC <uzbzarif@gmail.com>
2020-10-08 20:27:52 +0900

    MessageHendler

---
+++ echo.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+
+def hello(update, context):
+    update.message.reply_text('Hello World')
+
+
+updater = Updater(
+    '1324065101:AAFC8E5163G83YMG5MJ_X9ay1tvnmZ_6Mds', use_context=True)
+
+updater.dispatcher.add_handler(MessageHandler(hello))
+
+updater.start_polling()
+updater.idle()
