d8178cc2f2c5a692f594777a54609f655c8a4902
Zarif MAC <uzbzarif@gmail.com>
2020-10-10 20:13:00 +0900

    request contact from user

--- send_keyboard.py
+++ send_keyboard.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 import os
 
@@ -14,10 +14,14 @@ def hi(update, context):
 
 
 def start(update, context):
-
+    button = KeyboardButton(
+        text='contact',
+        request_contact=True
+    )
     reply_markup = ReplyKeyboardMarkup(
         [
-            ['Button1']
+            ['Button1'],
+            [button]
         ]
     )
     bot = context.bot
@@ -25,7 +29,7 @@ def start(update, context):
     chat_id = update.message.chat.id
     bot.sendMessage(
         chat_id=chat_id,
-        text='Welcome to our bot',
+        text='Share your Contact Phone',
         reply_markup=reply_markup
     )
 
@@ -37,13 +41,23 @@ def help_bot(update, context):
     bot.sendMessage(chat_id, 'This is just echo bot.')
 
 
+def button1(update, context):
+    bot = context.bot
+    text = update.message.text
+    chat_id = update.message.chat.id
+    bot.sendMessage(chat_id, 'Just Button 11111')
+
+
 updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('help', help_bot))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 
-updater.dispatcher.add_handler(MessageHandler(Filters.text, hi))
 
+updater.dispatcher.add_handler(MessageHandler(Filters.text, hi))
+updater.dispatcher.add_handler(
+    MessageHandler(Filters.text('Button1'),
+                   button1))
 
 updater.start_polling()
 updater.idle()
