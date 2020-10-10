111811805070d54e5a3e17e52bcaffe7cc183c48
Zarif MAC <uzbzarif@gmail.com>
2020-10-10 19:03:12 +0900

    flie_id

--- send_photo.py
+++ send_photo.py
@@ -3,10 +3,14 @@ import os
 import telegram
 
 TOKEN = os.environ['TOKEN']
-
+file_id = 'AgACAgIAAxkBAAICR1-BhdNuCnrC45xLHbJuv7sif1gsAAJNrzEbh8IJSAQPMhcKQq4xxSOlli4AAwEAAwIAA3gAA5dzAgABGwQ'
 bot = telegram.Bot(TOKEN)
+# file_photo = open('logo.png', 'rb')
+# print(type(file_photo))
 bot.sendPhoto(
     chat_id='86775091',
-    photo='https://random.dog/42e97d6a-c825-4191-9434-32cea191fc21.jpeg',
-    caption='DOG'
+    photo=file_id,
+    caption='CODESCHOOL LOGO'
 )
+# update = bot.getUpdates()[-1]
+# print(update.to_dict())
